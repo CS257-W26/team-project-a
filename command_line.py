420c597e09154b02c8b6f89fdf3da1a03ae73e54
Lloyd Stohler <lstohler22@tjs.org>
2026-01-22 15:24:16 -0600

    e

--- command_line.py
+++ command_line.py
@@ -3,8 +3,13 @@ The eventual location for the command line interface (CLI) for the project.
 This will be the entry point for the project when run from the command line.
 '''
 
-#FUNCTION 1
+'''Function for getting the data cooresponding to a specific country/date combination'''
 
+'''Function for getting the data in the column "Agricultural Water Use (%)"'''
+
+'''Function for getting the data in the column "Industrial Water Use (%)"'''
+
+'''Function for getting the data in the column "Household Water Use (%)"'''
 #FUNCTION 2
 
 #FUNCTION 3
