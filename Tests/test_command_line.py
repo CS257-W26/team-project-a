36e741ad3e2bca25d44d666ab4a10eaa236fefd3
Lloyd Stohler <lstohler22@tjs.org>
2026-01-23 12:22:34 -0600

    Started test work

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -1,1 +1,13 @@
+'''
+Test command line file
+'''
+
+import unittest
+from command_line import * #note, specify this late
+
+class CountryTests(unittest.TestCase):
+    def setUp(self):
+        main()
+
+    
 
