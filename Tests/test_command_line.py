1b0ef8556414f0aea40491dd267589ee07debb14
jungp12p <jungp@carleton.edu>
2026-01-25 16:41:58 -0600

    Test cases for f2

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -9,5 +9,20 @@ class CountryTests(unittest.TestCase):
     def setUp(self):
         main()
 
+    def test_per_capita_valid(self):
+        '''Test valid country and year inputs'''
+        result = get_per_capita_water_use("Japan", "2018")
+        self.assertAlmostEqual(result, 290.58, places=2)
+
+    def test_per_capita_invalid_country(self):
+        '''Test invalid country input'''
+        with self.assertRaises(ValueError):
+            get_per_capita_water_use("Wakanda", "2018")
+
+    def test_per_capita_invalid_year(self):
+        with self.assertRaises(ValueError):
+            get_per_capita_water_use("Japan", "1000")
+    
+    
     
 
