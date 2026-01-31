c78836fc8e6fbbbae4c57370cc864e7a12b30f95
Lloyd Stohler <lstohler22@tjs.org>
2026-01-31 14:13:42 -0600

    deleted one thing

--- ProductionCode/usage_proportion.py
+++ ProductionCode/usage_proportion.py
@@ -2,7 +2,7 @@
 Feature analysis functions for water usage data.
 """
 
-from ProductionCode.database import DB, filter_tags_database, filter_by_tags, open_database
+from ProductionCode.database import DB, filter_tags_database
 from ProductionCode.utils import alias
 
 def usage_proportion(country, year):
