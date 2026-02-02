fe359e5e59a31a7c3db1f169bb900df301b1b518
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-02-01 20:26:10 -0600

    Flask integration tests

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -1,7 +1,7 @@
 '''Tests flask app file'''
 
 import unittest
-from flask_app import home, water_use_route, usage_proportion_route, per_capita_route, page_not_found
+from flask_app import home, water_use_route, usage_proportion_route, per_capita_route, page_not_found, app, api
 
 
 class FlaskTest(unittest.TestCase):
@@ -14,7 +14,7 @@ class FlaskTest(unittest.TestCase):
     def test_404(self):
         """Tests 404 error works"""
         result = page_not_found("Argentina")
-        self.assertEqual(result,("Page not found", 404))
+        self.assertEqual(result,"Page not found. Try re-entering the link.")
 
     def test_proportion_flask_right(self):
         """Tests the intended Flask outcome for usage proportion"""
@@ -33,3 +33,43 @@ class FlaskTest(unittest.TestCase):
         result = water_use_route("usa", "2018", "2020")
         self.assertEqual(result,
             "Water usage in United States of America\n2018: 1829x10^9 cubic meters/year\n2020: 1829x10^9 cubic meters/year\nDifference:\n0x10^9 cubic meters/year")
+    
+
+
+
+class TestFlaskApp(unittest.TestCase):
+    """Tests for le flask app internal functionality."""
+    def setUp(self):
+        if 'api' not in app.blueprints:
+            app.register_blueprint(api, url_prefix='/api')
+
+    def run_test_site(self, url="/"):
+        """Helper function to quickly run the test flask site."""
+        cur_app = app.test_client()
+        return cur_app.get(url, follow_redirects=True)
+
+    def test_flask_app_homepage(self):
+        """Homepage test for flask"""
+        response = self.run_test_site("/")
+        self.assertIn("Welcome to the Global Water", str(response.data))
+
+    def test_flask_app_water_use(self):
+        """Homepage test for flask"""
+        response = self.run_test_site("/water_use/US/2003/2004")
+        self.assertIn("Water usage in United States of America\\n2003", str(response.data))
+
+
+    def test_get_row(self):
+        """Tests basic API function."""
+        response = self.run_test_site("/api/1/1/")
+        self.assertIn("30.613806827",str(response.data))
+
+    def test_get_row_col_invalid_row(self):
+        """Tests API error handling for nonexistant DB"""
+        response = self.run_test_site("/api/100000000/1")
+        self.assertIn("Could not find the requested",str(response.data))
+
+    def test_get_row_col_invalid_col(self):
+        """Tests API error handling for nonexistant row"""
+        response = self.run_test_site("/api/1/10000000000")
+        self.assertIn("Could not find the requested",str(response.data))
--- flask_app.py
+++ flask_app.py
@@ -16,7 +16,7 @@ def home():
     '''Home route'''
     return "Welcome to the Global Water Sources v Spending website!"
 
-@app.route('/water_use/<country>/<year1>/<year2>')
+@app.route('/water_use/<country>/<year1>/<year2>/')
 def water_use_route(country, year1, year2):
     try:
         result = water_use_time_compare(country, year1, year2)
@@ -25,7 +25,7 @@ def water_use_route(country, year1, year2):
         return str(e)
 
 
-@app.route('/usage_proportion/<country>/<year>')
+@app.route('/usage_proportion/<country>/<year>/')
 def usage_proportion_route(country, year):
     '''Usage proportion route'''
     try:
@@ -34,7 +34,7 @@ def usage_proportion_route(country, year):
     except ValueError as e:
         return str(e)
 
-@app.route('/per_capita/<country>/<year>')
+@app.route('/per_capita/<country>/<year>/')
 def per_capita_route(country, year):
     '''Per capita water use route'''
     try:
@@ -43,7 +43,7 @@ def per_capita_route(country, year):
     except ValueError as e:
         return str(e)
 
-@api.route('/<int:database_id>/<int:row>')
+@api.route('/<int:database_id>/<int:row>/')
 def get_database_row(database_id,row):
     """API route for directly accessing the database."""
     try:
