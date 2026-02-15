5576789b8e4dac9cd7cd46a3c19f141a7331b561
Lloyd Stohler <lstohler22@tjs.org>
2026-02-15 16:52:21 -0600

    rkenrr

--- ProductionCode/untitled2.py
+++
@@ -1,4 +0,0 @@
-from database import *
-databite = "SELECT agr_total FROM GLOBALDATA_S WHERE yr = " + '2004' + " AND country = ’"+ 'France' +"’;"
-data2 = DataSource.run_string_psql(databite)
-print(data2)
