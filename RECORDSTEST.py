68aadfdaf8c9c9b5a69d454cfe1cbc57bb04a6d0
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-02-15 15:12:05 -0600

    Test file

---
+++ RECORDSTEST.py
@@ -0,0 +1,9 @@
+from ProductionCode.database import DataSource
+
+
+def main():
+    source = DataSource()
+    source.run_string_psql("SELECT * FROM AQTE")
+
+if __name__ == "__main__":
+    main()
