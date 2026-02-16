83774944b4f3d9a0af001545e9504a602d9f9957
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-02-15 21:46:45 -0600

    test

--- Tests/test_flask_app.py
+++ Tests/test_flask_app.py
@@ -66,7 +66,7 @@ class TestFlaskApp(unittest.TestCase):
     def test_flask_app_404(self):
         """404 test for flask"""
         response = self.run_test_site("/WEWLAD/")
-        self.assertIn("404 - Page Not Found", str(response.data))
+        self.assertIn("to complete your request", str(response.data))
 
     def test_flask_app_water_use_us(self):
         """Water use test for US"""
