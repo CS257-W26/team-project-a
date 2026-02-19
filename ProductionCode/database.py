5589783f38e4f9e5bf064d70c2015e1d5a4812ee
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-02-19 14:58:01 -0600

    i dont know man

--- ProductionCode/database.py
+++ ProductionCode/database.py
@@ -21,7 +21,7 @@ class DataSource: #TODO: Replace this so that ONLY datasource manages SQL (ie it
         Prints the result of that command being run
         '''
         result = self.db.query(str_command)
-        if len(result) > 0:
+        if len(result.all()) > 0:
             return result[0]
         raise IndexError
 
