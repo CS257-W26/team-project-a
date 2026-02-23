2cd83bb048b8cfdc6f1b5494f33ae2a5bbd2c08a
Lloyd Stohler <lstohler22@tjs.org>
2026-02-23 08:47:59 -0600

    str issue

--- ProductionCode/per_capita.py
+++ ProductionCode/per_capita.py
@@ -12,7 +12,7 @@ def get_per_capita_water_use(country: str, year: str) -> float:
     @param country: The country to get per capita water use for
     @param year: The year to get per capita water use for
     @return: Per capita water use in liters per day"""
-    if not year.isdigit() or not 2000 <= int(year) <= 2024:
+    if not str(year).isdigit() or not 2000 <= int(year) <= 2024:
         raise ValueError("Year must be between 2000 and 2024.")
 
     country = alias(country)
